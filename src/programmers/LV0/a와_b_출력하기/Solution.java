0ae19443cdd94272cf1d143e6929b5c2ecb78fe4
sayu95 <jeongsayu95@gmail.com>
2025-07-30 10:09:07 +0900

    20250730

---
+++ src/programmers/LV0/a와_b_출력하기/Solution.java
@@ -0,0 +1,15 @@
+package programmers.LV0.a와_b_출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " +a);
+        System.out.println("b = " +b);
+
+    }
+}
