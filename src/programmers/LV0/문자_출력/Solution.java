00aa1b5bbf63a84d93d631dae8a76b2e623c05eb
sayu95 <jeongsayu95@gmail.com>
2025-07-30 10:06:17 +0900

    20250730

---
+++ src/programmers/LV0/문자_출력/Solution.java
@@ -0,0 +1,9 @@
+package programmers.LV0.문자_출력;
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
---
+++ src/programmers/LV0/피타고라스의_정리/Solution.java
@@ -0,0 +1,14 @@
+package programmers.LV0.피타고라스의_정리;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+        int b_square = (int) Math.pow(c, 2) - (int) Math.pow(a, 2);
+
+        System.out.println(b_square);
+    }
+}
