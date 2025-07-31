c39e0fa0d82ff83b57e57e16dd03094b3743b955
sayu95 <jeongsayu95@gmail.com>
2025-07-31 09:17:02 +0900

    20250731

---
+++ src/programmers/LV0/문자열_붙여서_출력하기/Solution.java
@@ -0,0 +1,14 @@
+package programmers.LV0.문자열_붙여서_출력하기;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String str;
+        str = a + b;
+        System.out.println(str);
+    }
+}
