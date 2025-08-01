a0c86d47ad37e2264630be3ca4749a5ac7f605f7
sayu95 <jeongsayu95@gmail.com>
2025-08-01 22:34:05 +0900

    20250801

---
+++ src/programmers/LV0/문자_리스트를_문자열로_변환하기/Solution.java
@@ -0,0 +1,11 @@
+package programmers.LV0.문자_리스트를_문자열로_변환하기;
+
+class Solution {
+    public String solution(String[] arr) {
+        String answer = "";
+        for (String str : arr) {
+            answer += str;
+        }
+        return answer;
+    }
+}
