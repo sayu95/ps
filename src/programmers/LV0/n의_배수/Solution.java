2084f93c544016050aedd7e65e77e07b54a08d41
sayu95 <jeongsayu95@gmail.com>
2025-08-02 09:20:11 +0900

    20250802

--- src/programmers/LV0/n의_배수/Solution.java
+++ src/programmers/LV0/n의_배수/Solution.java
@@ -3,7 +3,7 @@ package programmers.LV0.n의_배수;
 class Solution {
     public int solution(int num, int n) {
         int answer = 0;
-        if(num % n == 0) answer = 1;
+        if (num % n == 0) answer = 1;
         return answer;
     }
 }
---
+++ src/programmers/LV0/공배수/Solution.java
@@ -0,0 +1,9 @@
+package programmers.LV0.공배수;
+
+class Solution {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+        if (number % n == 0 && number % m == 0) answer = 1;
+        return answer;
+    }
+}
