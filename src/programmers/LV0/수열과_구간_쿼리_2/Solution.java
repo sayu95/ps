2e049c58da2697312979b9310a57407e748acd76
sayu95 <jeongsayu95@gmail.com>
2025-08-04 21:02:33 +0900

    20250804

---
+++ src/programmers/LV0/수열과_구간_쿼리_2/Solution.java
@@ -0,0 +1,24 @@
+package programmers.LV0.수열과_구간_쿼리_2;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            int min = Integer.MAX_VALUE;
+            for (int j = s; j <= e; j++) {
+                if (arr[j] < min && k < arr[j]) {
+                    min = arr[j];
+                }
+            }
+            if (min != Integer.MAX_VALUE) {
+                answer[i] = min;
+            } else {
+                answer[i] = -1;
+            }
+        }
+        return answer;
+    }
+}
