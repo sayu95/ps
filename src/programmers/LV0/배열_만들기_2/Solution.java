05189282efdcc01e955ae23a9c9574de127dac4f
sayu95 <jeongsayu95@gmail.com>
2025-08-05 20:06:51 +0900

    20250805

---
+++ src/programmers/LV0/배열_만들기_2/Solution.java
@@ -0,0 +1,21 @@
+package programmers.LV0.배열_만들기_2;
+
+import java.util.*;
+
+class Solution {
+    public List solution(int l, int r) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = l; i <= r; i++) {
+            boolean b = true;
+            String str = String.valueOf(i);
+            for (char c : str.toCharArray()) {
+                if (c != '5' && c != '0') {
+                    b = false;
+                }
+            }
+            if (b) answer.add(i);
+        }
+        if (answer.isEmpty()) answer.add(-1);
+        return answer;
+    }
+}
