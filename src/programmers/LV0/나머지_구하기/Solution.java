5bee371def1025b128eafe061cf71d2773f05521
sayu95 <jeongsayu95@gmail.com>
2025-08-22 23:37:56 +0900

    20250822

---
+++ src/programmers/LV0/나머지_구하기/Solution.java
@@ -0,0 +1,9 @@
+package programmers.LV0.나머지_구하기;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
+    }
+}
