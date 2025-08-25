f7c0b7ad002223b79cacb21c375d669d46cdfb17
sayu95 <jeongsayu95@gmail.com>
2025-08-25 23:35:08 +0900

    20250825

---
+++ src/backjoon/Main.java
@@ -0,0 +1,13 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int num = a - b;
+        System.out.println(num);
+    }
+}
