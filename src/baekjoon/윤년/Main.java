382a2e21f523b653e2e6a6d2d04bd89c12c52066
sayu95 <jeongsayu95@gmail.com>
2025-09-04 00:40:23 +0900

    20250904

--- src/baekjoon/윤년/Main.java
+++ src/baekjoon/윤년/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		year = scan.nextInt();
 		//4의 배수이면서 100의 배수가 아니라서 윤년
-		if(year%4==0) {
+		if(year%4==0) {								 //로직이 지저분함
 			if(year%100!=0) System.out.println("1");
 		} else System.out.println("0");
 		if(year%100 == 0) {
--- src/baekjoon/주사위_세개/Main.java
+++ src/baekjoon/주사위_세개/Main.java
@@ -1,32 +1,36 @@
 package baekjoon.주사위_세개;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int non1 = Integer.parseInt(br.readLine());
-        int non2 = Integer.parseInt(br.readLine());
-        int non3 = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int non1 = Integer.parseInt(st.nextToken());
+        int non2 = Integer.parseInt(st.nextToken());
+        int non3 = Integer.parseInt(st.nextToken());
         int result = 0;
-        //같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
-        if(non1 == non2 && non2 == non3) result = 10000 + non1 * 1000;
-        //같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
-        if(non1 == non2 && non2 != non3) result = 1000 + non1 * 100;
+        if (non1 == non2 && non2 == non3) result = 10000 + non1 * 1000; //로직이 지저분함
+        if (non1 == non2 && non2 != non3) result = 1000 + non1 * 100;
         else if (non1 == non3 && non2 != non1) result = 1000 + non1 * 100;
-        else if (non2 == non3 && non2 == non1) result = 1000 + non1 * 100;
-        //모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
-        if(non1 != non2 && non2 != non3) {
-            int max = non1;
-            if(max < non2) {
-                max = non2;
-                bw.write(max+"");
-            }
-            if(max < non3) {
-                max = non3;
-                bw.write(max+"");
+        else if (non2 == non3 && non1 != non2) result = 1000 + non2 * 100;
+        if (non1 != non2 && non2 != non3) {
+            if(non1 != non3) { //Math.max 함수 쓰기
+                int max = non1;
+                if (max < non2) {
+                    max = non2;
+                }
+                if(max < non3) {
+                    max = non3;
+                }
+                result = max * 100;
             }
         }
+        bw.write(result+"");
+        bw.flush();
+        br.close();
+        bw.close();
     }
 }
