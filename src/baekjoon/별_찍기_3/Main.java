0808b2d6f7fa8f9db2edd08614d5c2e948010430
sayu95 <jeongsayu95@gmail.com>
2025-09-13 11:30:53 +0900

    20250913

---
+++ src/baekjoon/별_찍기_3/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.별_찍기_3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int star = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=star; i++) { //행 출력
+			for(int j=star; j>=i; j--) {  // 별 출력
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
