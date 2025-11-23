3afca87c71cb3b9bffd323980b1153620ccd7198
sayu95 <jeongsayu95@gmail.com>
2025-11-23 22:41:40 +0900

    *숫자_10093

--- BaekJoon/숫자_10093.py
+++ BaekJoon/숫자_10093.py
@@ -1,9 +1,12 @@
 num1, num2 = map(int, input().split())
 maxNum = max(num1, num2)
 minNum = min(num1, num2)
+
 cnt = maxNum - minNum -1
+
 if maxNum - minNum <= 1:
     cnt = 0
+    
 print(cnt)
 for i in range(minNum+1, maxNum) :
     print(i, end=" ")
