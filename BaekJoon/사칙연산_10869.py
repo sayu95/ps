b6fc4df7d56a6122ae4e85b2369b5047c5cfddd4
sayu95 <jeongsayu95@gmail.com>
2025-11-27 00:10:35 +0900

    사칙연산_10869

--- BaekJoon/사칙연산_10869.py
+++ BaekJoon/사칙연산_10869.py
@@ -1,10 +1,7 @@
 num1, num2 = map(int, input().split())
 
-def cal(num1, num2) :
-    print(num1+num2)
-    print(num1-num2)
-    print(num1*num2)
-    print(int(num1/num2))
-    print(int(num1%num2))
-
-cal(num1, num2)
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(int(num1/num2))
+print(int(num1%num2))
